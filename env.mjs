262793427f3f963b880c87439af643076560e46d
Sattwik <hellosattwik@gmail.com>
2023-06-08 09:32:54 +0530

    added stripe publishable key

--- env.mjs
+++ env.mjs
@@ -5,9 +5,11 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     STRIPE_SECRET_KEY: z.string(),
+    STRIPE_PUBLISHABLE_KEY: z.string(),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
   },
 });
