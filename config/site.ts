0b71b21672090e5fad10ceaa77e37e8c9d2a8047
Sattwik <hellosattwik@gmail.com>
2023-06-08 18:47:34 +0530

    add discord link

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,13 @@
-import { SiteConfig } from '@/types';
+import { SiteConfig } from "@/types"
+
 export const siteConfig = {
-  name: 'Cliently',
+  name: "Cliently",
   description:
-    'Effortlessly Manage Clients and Projects: Streamline your business operations with our all-in-one client management platform. Simplify team collaboration, track projects, handle billing, and automate workflows seamlessly. Experience the power of efficiency and take control of your business growth. Get started now!',
-  ogImage: '',
-  url: 'https://cliently.xyz',
+    "Effortlessly Manage Clients and Projects: Streamline your business operations with our all-in-one client management platform. Simplify team collaboration, track projects, handle billing, and automate workflows seamlessly. Experience the power of efficiency and take control of your business growth. Get started now!",
+  ogImage: "",
+  url: "https://cliently.xyz",
   links: {
-    twitter: 'https://twitter.com/sattwyk',
-    github: 'https://github.com/sattwyk',
+    twitter: "https://twitter.com/sattwyk",
+    discord: "https://discord.gg/fJDeEScuZD",
   },
-} satisfies SiteConfig;
+} satisfies SiteConfig
