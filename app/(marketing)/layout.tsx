1399246af6f670e5360edfc2c3725f0ecfc22979
Sattwik <hellosattwik@gmail.com>
2023-06-08 19:58:39 +0530

    add meta export in root layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link"
 
 import { marketingConfig } from "@/config/marketing"
-import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
@@ -9,63 +8,6 @@ import { SiteFooter } from "@/components/site-footer"
 
 interface MarketingLayoutProps {
   children: React.ReactNode
-}
-
-export const metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  keywords: [
-    "Client management",
-    "Project management",
-    "Team collaboration",
-    "Billing automation",
-    "Workflow automation",
-    "Business efficiency",
-    "Project tracking",
-    "Client communication",
-    "Task management",
-    "Time tracking",
-    "Invoicing",
-    "CRM",
-    "Team productivity",
-    "Business growth",
-    "Streamline operations",
-  ],
-  authors: [
-    {
-      name: "sattwyk",
-      url: "https://sattwyk.com",
-    },
-  ],
-  creator: "sattwyk",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/og.jpg`],
-    creator: "@sattwyk",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
 }
 
 export default async function MarketingLayout({
