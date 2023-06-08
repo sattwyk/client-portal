d718d5c44f5106e941cabdbb1161c170e191a988
Sattwik <hellosattwik@gmail.com>
2023-06-08 09:39:29 +0530

    remove appInfo

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,9 +3,4 @@ import { env } from '@/env.mjs';
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: '2022-11-15',
-  appInfo: {
-    name: 'nuvvo',
-    version: '0.0.1',
-    url: 'localhost:3000',
-  },
 });
