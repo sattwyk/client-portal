04ecdebf7887939f64e3b88837d5d13c33320079
Sattwik <hellosattwik@gmail.com>
2023-06-08 18:50:16 +0530

    minor footer changes

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,58 +1,45 @@
-import * as React from 'react';
+import * as React from "react"
+import { Copyright } from "lucide-react"
 
-import { siteConfig } from '@/config/site';
-import { cn } from '@/lib/utils';
-import { Icons } from '@/components/icons';
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+
 // import { ModeToggle } from '@/components/mode-toggle';
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <div className='container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0'>
-        <div className='flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0'>
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo />
-          <p className='text-center text-sm leading-loose md:text-left'>
-            Built by{' '}
+          <p className="text-center text-sm leading-loose md:text-left">
+            Made with 🖤 in{" "}
             <a
-              href={siteConfig.links.twitter}
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
+              href="https://www.thetimes.co.uk/travel/destinations/asia/india/best-places-to-visit-in-india"
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
             >
-              sattwyk
-            </a>
-            . Hosted on{' '}
-            <a
-              href='https://vercel.com'
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-            >
-              Vercel
-            </a>
-            . Illustrations by{' '}
-            <a
-              href='https://popsy.co'
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-            >
-              Popsy
-            </a>
-            . The source code is available on{' '}
-            <a
-              href={siteConfig.links.github}
-              target='_blank'
-              rel='noreferrer'
-              className='font-medium underline underline-offset-4'
-            >
-              GitHub
+              India
             </a>
             .
           </p>
         </div>
         {/* <ModeToggle /> */}
+        <p className="text-center text-sm leading-loose md:text-left">
+          Join our{" "}
+          <a
+            href={siteConfig.links.discord}
+            target="_blank"
+            rel="noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            Discord
+          </a>{" "}
+          Community.
+        </p>
       </div>
     </footer>
-  );
+  )
 }
