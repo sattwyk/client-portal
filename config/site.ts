b77fbf78da3770a69796d3a071caef99404423ce
Sattwik <hellosattwik@gmail.com>
2023-06-08 17:18:45 +0530

    add site config

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from '@/types';
+export const siteConfig = {
+  name: 'Cliently',
+  description:
+    'Effortlessly Manage Clients and Projects: Streamline your business operations with our all-in-one client management platform. Simplify team collaboration, track projects, handle billing, and automate workflows seamlessly. Experience the power of efficiency and take control of your business growth. Get started now!',
+  ogImage: '',
+  url: 'https://cliently.xyz',
+  links: {
+    twitter: 'https://twitter.com/sattwyk',
+    github: 'https://github.com/sattwyk',
+  },
+} satisfies SiteConfig;
